b5e8a3ead94587119f448d54ccc50677d1bb93b0
TRAN-cd <ngockiet.tran11@gmail.com>
2026-01-22 21:35:47 +0900

    feat: STEP2 画面構成作成

---
+++ src/components/ProjectCreatePage.tsx
@@ -0,0 +1,10 @@
+const ProjectCreatePage = () => {
+  return (
+    <div>
+      <h1>プロジェクト作成</h1>
+      <p>※ 今回はUIのみ</p>
+    </div>
+  )
+}
+
+export default ProjectCreatePage
---
+++ src/components/ProjectListPage.tsx
@@ -0,0 +1,14 @@
+const ProjectListPage = () => {
+  return (
+    <div>
+      <h1>プロジェクト一覧</h1>
+
+      <ul>
+        <li>会社A コーポレートサイト</li>
+        <li>会社B LP制作</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ProjectListPage
